309c2c0ac37ff7b35e29e6bc5c4b4721096088dd
vyanezf <vyanezf@gmail.com>
2023-12-27 18:08:16 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,48 +1,35 @@
+const audioCtx = new AudioContext();
+if (navigator.mediaDevices) {
+  navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+    const source = audioCtx.createMediaStreamSource(stream);
+     // Connect source to RNBO Device
+     source.connect(device.node);
+  
+     // Connect RNBO Device to destination output
+     device.node.connect(audioCtx.destination);
+    // `microphone` can now act like any other AudioNode
+  }).catch((err) => {
+    // browser unable to access microphone
+    // (check to see if microphone is attached)
+  });
+} else {
+  // browser unable to access media devices
+  // (update your browser)
+}
 (async () => {
-    const ctx = new (window.AudioContext || window.webkitAudioContext)();
-    const response = await fetch(
-      "export/patch.export.json"
-    );
-    const patcher = await response.json();
-    let setup = false;
-  
-    const device = await RNBO.createDevice({
-      context: ctx,
-      patcher
-    });
-  
-    const run = async () => {
-      if (setup) return;
-      setup = true;
-      ctx.resume();
-  
-      try {
-        // Access user's microphone
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: false
-        });
-  
-        // Create Media Stream source
-        const source = ctx.createMediaStreamSource(stream);
-  
-        // Connect source to RNBO Device
-        source.connect(device.node);
-  
-        // Connect RNBO Device to destination output
-        device.node.connect(ctx.destination);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-  
-    const onGainChange = (e) =>
-      (device.parametersById.get(
-        e.currentTarget.name
-      ).normalizedValue = parseFloat(e.currentTarget.value));
-  
-    document.querySelector("#start").onclick = run;
-    document.querySelector("#gain").onchange = onGainChange;
-  })();
-  
-  
+const response = await fetch("export/patch.export.json");
+const patcher = await response.json();
+let setup = false;
+
+const device = await RNBO.createDevice ({
+  context: audioCtx,
+  patcher
+}); 
+const onGainChange = (e) =>
+(device.parametersById.get(
+  e.currentTarget.name
+).normalizedValue = parseFloat(e.currentTarget.value));
+
+document.querySelector("#start").onclick = run;
+document.querySelector("#gain").onchange = onGainChange;
+});
